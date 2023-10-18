8f18050c47c1e2f0f4907cf6ffeee5f0168ea2a9
md omer arafat <arafatomer66@gmail.com>
2023-10-18 21:29:32 +0600

    migration files added

--- seeds/users.js
+++ seeds/users.js
@@ -1,11 +1,28 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = function(knex) {
-  return knex('users').insert([
-    { username: 'user1', email: 'user1@example.com' },
-    { username: 'user2', email: 'user2@example.com' },
-    // Add more data as needed
-  ]);
+exports.seed = function (knex) {
+  // Deletes ALL existing entries in the "users" table
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries into the "users" table
+      return knex('users').insert([
+        {
+          username: 'user1',
+          email: 'user1@example.com',
+          created_at: new Date(),
+          bio: 'User 1 Bio',
+          phone: '123-456-7890',
+          password: 'password1',
+          status: 'active',
+        },
+        {
+          username: 'user2',
+          email: 'user2@example.com',
+          created_at: new Date(),
+          bio: 'User 2 Bio',
+          phone: '987-654-3210',
+          password: 'password2',
+          status: 'inactive',
+        },
+        // Add more seed data as needed
+      ]);
+    });
 };
