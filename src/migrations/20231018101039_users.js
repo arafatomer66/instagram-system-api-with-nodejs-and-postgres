41b999693539b473fddde39f8411be0662ea4207
arafatomer66 <arafatomer66@gmail.com>
2023-10-19 11:04:14 +0600

    users table and sql added for table creation

--- src/migrations/20231018101039_users.js
+++ src/migrations/20231018101039_users.js
@@ -2,19 +2,38 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+// Create Table users (
+//     id serial primary key,
+//     created_at timestamp with time zone Default current_timestamp,
+//     updated_at timestamp with time zone Default current_timestamp,
+//     username varchar(30) not null,
+//     bio varchar(400)  not null default '',
+// 	avatar varchar(200),
+// 	phone varchar(25),
+// 	email varchar(15),
+// 	status varchar(15),
+// 	password varchar(50)
+// 	check(coalesce(phone, email) is not null),
+// 	check(coalesce(email, password) is not null), 
+// )
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
-        table.increments('id').primary();
-        table.string('username');
-        table.string('email');
-        // Add any other columns and constraints you need for your "users" table
-    });
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+      table.increments('id').primary();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+      table.string('username', 30).notNullable();
+      table.string('bio', 400).notNullable().defaultTo('');
+      table.string('avatar', 200);
+      table.string('phone', 25);
+      table.string('email', 15);
+      table.string('status', 15);
+      table.string('password', 50);
+    })
+    .raw('ALTER TABLE users ADD CONSTRAINT coalesce_check1 CHECK (coalesce(phone, email) is not null)')
+    .raw('ALTER TABLE users ADD CONSTRAINT coalesce_check2 CHECK (coalesce(email, password) is not null)');
+  };
+  
+  exports.down = function(knex) {
     return knex.schema.dropTable('users');
-};
+  };
